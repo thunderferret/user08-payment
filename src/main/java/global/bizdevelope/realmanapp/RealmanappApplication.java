e92e62239662d51564386533a3b975ef210a9341
SGT SEO <vpflt992@gmail.com>
2021-05-29 22:18:47 +0900

    Initial Commit

---
+++ src/main/java/global/bizdevelope/realmanapp/RealmanappApplication.java
@@ -0,0 +1,13 @@
+package global.bizdevelope.realmanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealmanappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealmanappApplication.class, args);
+    }
+
+}
